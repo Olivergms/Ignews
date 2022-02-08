6a159bc0b50ec1e4b1bda3fdd9758eab7bc21ce9
Adriano Gomes de Oliveira <oliveradriano.ads@gmail.com>
2022-02-07 21:01:32 -0300

    Pagina home criada.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,26 @@
-
 import Head from 'next/head'
+import { SubscribeButton } from '../components/SubscribeButton';
+import styles from './home.module.scss';
+
 
 export default function Home() {
   return (
     <>
     <Head>
-         <title>Início | ig.news</title>
+         <title>Home | ig.news</title>
        </Head>
-    <h1>
-       Hello world
-       
-    </h1>
+    <main className={styles.contentContainer}>
+      <section className={styles.hero}>
+        <span>👏 Hey, welcome</span>
+        <h1>News about the <span>React</span> world.</h1>
+        <p>
+          Get acess to all the publications <br/>
+          <span>For $9.90 mounth</span>
+        </p>
+        <SubscribeButton />
+      </section>
+      <img src="/images/avatar.svg" alt="Girl coding" />
+    </main>
     </>
   )
 }
